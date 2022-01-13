69755939c83eeb75db5a3094cae6fdf32029cbd1
Andrew <andrew.kapsa1@gmail.com>
2022-01-12 20:51:41 -0500

    commit tree b4702e7922e1536efdf736a84919ded60f01ec49

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// code your solution here
+function saturdayFun(activity){
+   if (activity == undefined) {
+return `This Saturday, I want to roller-skate!`
+   }
+    else{
+        return `This Saturday, I want to ${activity}!`
+    }
+}
+const mondayWork = function(activity2){
+    if (activity2 == undefined) {
+        return "This Monday, I will go to the office.";
+    }
+    else {
+        return `This Monday, I will ${activity2}.`;
+    }
+}
+function wrapAdjective(Catch="*"){
+    return function inner(adjective ="awesome"){
+        return (`You are ${Catch}${adjective}${Catch}!`)
+    }
+}
